eb8ebe53fee1a57b258fd0a47fc5c1993999b67d
Vanex-h <vanessahirwa5@gmail.com>
2024-06-24 15:31:38 +0400

    .

--- JsonPlaceHolder/components2/CreatePost.tsx
+++ JsonPlaceHolder/components2/CreatePost.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet, Alert } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Alert, Text } from 'react-native';
 import { createPost } from '../services/api';
-
+import tw from "twrnc";
 interface CreatePostProps {
   onPostCreated: () => void;
 }
 
 export const CreatePost: React.FC<CreatePostProps> = ({ onPostCreated }) => {
   const [title, setTitle] = useState('');
+  const [error, setError] = useState("");
   const [body, setBody] = useState('');
 
   const handleCreatePost = async () => {
@@ -24,17 +25,46 @@ export const CreatePost: React.FC<CreatePostProps> = ({ onPostCreated }) => {
 
   return (
     <View style={styles.container}>
+       {error && <Text style={tw`text-red-500 mb-2`}>{error}</Text>}
       <TextInput
         style={styles.input}
         placeholder="Title"
         value={title}
         onChangeText={setTitle}
+        onBlur={() => {
+          // Last name validation logic here
+          if (title.length < 3 || title.trim() === "") {
+              // Handle invalid last name input here
+              setError(
+                  "Title must be at least 3 characters long and not empty"
+              );
+              setTimeout(() => {
+                  setError("");
+                }, 1000);
+          } else {
+              setError("");
+          }
+      }}
       />
       <TextInput
         style={styles.input}
         placeholder="Body"
         value={body}
         onChangeText={setBody}
+        onBlur={() => {
+          // Last name validation logic here
+          if (body.length < 3 || body.trim() === "") {
+              // Handle invalid last name input here
+              setError(
+                  "Title must be at least 3 characters long and not empty"
+              );
+              setTimeout(() => {
+                  setError("");
+                }, 1000);
+          } else {
+              setError("");
+          }
+      }}
         multiline
       />
       <Button title="Create Post" onPress={handleCreatePost} />
